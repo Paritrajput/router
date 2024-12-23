d1aac1906a3db77df12d32c0d218f2a009039f8c
Paritrajput <159407740+Paritrajput@users.noreply.github.com>
2024-12-23 10:48:47 +0530

    Update Profile.jsx

--- src/component/MyProfile/Profile.jsx
+++ src/component/MyProfile/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../Context/userContext";
 
 import { useNavigate } from "react-router-dom";
-import { get } from "mongoose";
+
 
 function Profile() {
   const [username, setUsername] = useState("Guest");
